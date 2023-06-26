ba05ad263b4c0c2bef7cb9bdda9ad8e27ab2501b
2DC <92588154+2duckchun@users.noreply.github.com>
2023-06-26 14:46:47 +0900

    update : morethanlog site.config.js 세팅

--- site.config.js
+++ site.config.js
@@ -1,25 +1,29 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "2DC",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    bio: "I'm currently broadening my knowledge.",
+    email: "kkts9308@gmail.com",
+    linkedin: "",
+    github: "2duckchun",
+    instagram: "https://www.instagram.com/kts9308/",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `momovivie`,
+      href: "https://github.com/2duckchun/momovivie",
+    },
+    {
+      name: `deverymarkit`,
+      href: "https://github.com/deverymarkit",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "2DC-log",
+    description: "welcome to my Log!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
@@ -51,7 +55,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
